104b1b5617c8388eb2622c80b7ef7cbdde26c6ca
mtcho <myintthwaycho11@gmail.com>
2025-08-06 12:50:22 +0700

    implement a button that prompts user for the number of squares per side for the new grid and replace the old one

--- scripts.js
+++ scripts.js
@@ -14,3 +14,28 @@ const sq_divs = document.querySelectorAll(".square");
 for(const sq_div of sq_divs){
     sq_div.addEventListener("mouseenter",changeColor);
 }
+
+function createNewSketchPad(){
+    
+    let size = prompt('Plz enter your new sketch pad dimension:\n(e.g.16=16x16, 24=24x24)\nNote:Maximum limit is 100!');
+    
+    while(isNaN(size)||size <=0 || size >100){
+        size = prompt('Make sure you are using a number between 1 and 100!');
+    }
+
+    container.innerHTML=''; //clear the existing grids
+
+    for (let i = 1; i <= size*size;i++){ //generate new grid with user's desired grid count
+        const sq = document.createElement("div");
+        
+        sq.style.flex= `0 0 calc(100% / ${size})`;
+        sq.style.aspectRatio= '1 / 1';
+        sq.style.boxSizing = 'border-box';
+        sq.style.border = "1px solid #ccc";
+        sq.addEventListener('mouseenter',changeColor);
+        container.appendChild(sq);
+    }
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click',createNewSketchPad);
